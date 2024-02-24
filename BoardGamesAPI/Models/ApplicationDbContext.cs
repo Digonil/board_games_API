a592653ba76921e2538f04ae009f430726c3fded
Diego Niles <diego.niles@hotmail.com>
2024-02-24 19:31:36 -0300

    Adicionado a Classe BoardGame e configurado o contexto para o banco de dados

---
+++ BoardGamesAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BoardGamesAPI.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ BoardGamesAPI/Models/BoardGame.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoardGamesAPI.Models;
+
+[Table("BoardGames")]
+public class BoardGame
+{
+    public  int  Id { get; set; }
+    public string Name  { get; set; }
+    public int Year { get; set; }
+    public int MinPlayers { get; set; }
+    public int MaxPlayers { get; set; }
+    public int PlayTime { get; set; }
+    public int MinAge { get; set; }
+    public int UsersRated { get; set; }
+    public decimal RatingAverage { get; set; }
+    public int BGGRank { get; set; }
+    public decimal ComplexityAverage { get; set; }
+    public int OwnedUsers { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime LastModifiedDate { get; set; }
+}
