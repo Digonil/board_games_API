9d0f244afd837420aad946f71b795fa0b62833b7
Diego Niles <diego.niles@hotmail.com>
2024-02-26 21:09:28 -0300

    Criado a classe Domain

---
+++ BoardGamesAPI/Models/Domain.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BoardGamesAPI.Models;
+
+[Table("Domains")]
+public class Domain
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    
+    [Required]
+    public string? Name { get; set; }
+
+    [Required]
+    public DateTime CreatedDate { get; set; }
+
+    [Required]
+    public DateTime LastModifiedDate { get; set; }
+}
